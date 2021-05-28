efdda5b0006ddb3f4f0c938eb84e4a7b3fd5ad92
Janice-M <janiceink001@gmail.com>
2021-05-28 23:15:25 +0300

    Athens vs Sparta

--- vanilla/src/checkoutForm.jsx
+++ vanilla/src/checkoutForm.jsx
@@ -4,5 +4,11 @@ import { CardElement, useStripe, useElements} from "@stripe/react-stripe-js";
 export default function CheckoutForm(){
     const [succeeded, serSucceeded ] = useState(false);
     const [error , setError ] = useState(null);
-    const [processing, setProcessing] = useState ()
+    const [processing, setProcessing] = useState ('');
+    const [disabled, setDisabled] = useState(true);
+    const [clientSecret, setClientSecret] = useState('');
+    const stripe = useStripe();
+    const elements = useElements();
+
+    useEffect
 }
